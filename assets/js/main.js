5f1b3dfd5263bec827060d68dbdd35a75571cfba
m1well <michaelwellner@gmx.de>
2020-01-05 13:43:13 +0100

    main update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction()
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("back-to-top").style.display = "block";
   } else {
     document.getElementById("back-to-top").style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+function scrollToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
