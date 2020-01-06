b1baffdbe9b017ecd394227d08108fa80ffec233
m1well <michaelwellner@gmx.de>
2020-01-06 11:28:59 +0100

    added new elements and new skills and technologies cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,17 @@ function scrollFunction() {
 }
 
 function scrollToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  const position = document.body.scrollTop || document.documentElement.scrollTop;
+  let scrollAnimation = 0;
+  if (position) {
+    window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
+    scrollAnimation = setTimeout("scrollToTop()", 20);
+  } else {
+    clearTimeout(scrollAnimation);
+  }
 }
+
+let xmlHttp = new XMLHttpRequest();
+xmlHttp.open('GET', 'https://hitcounter.pythonanywhere.com/count', false);
+xmlHttp.send(null);
+console.log(xmlHttp.responseText);
